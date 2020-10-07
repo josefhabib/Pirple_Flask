9c97ef29045abb2eb22a46e7a274bff1126e353f
Josef Habib <josef.habib2@gmail.com>
2020-10-07 16:50:39 +0100

    homework02

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('helloWorld.html')
+    return render_template('landing.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
 
 if __name__=='__main__':
     app.run(port=7000, debug=True)
